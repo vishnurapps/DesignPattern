c0fd98d86eb4322f4009fef1bf2bbbc3fc933b5b
Vishnu R <vishnurapps@gmail.com>
2018-08-28 23:38:46 +0530

    Strategy design pattern

---
+++ behavioral-patterns/strategy/strategy.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <memory>
+
+class Vehicle
+{
+public:
+	virtual void useVehicle();
+};
+
+class HyundaiI20 : public Vehicle
+{
+public:
+	void useVehicle()
+	{
+		std::cout << "Driving Hyundai I20" << std::endl;
+	}
+};
+
+class MaruthiSwift : public Vehicle
+{
+public:
+	void useVehicle()
+	{
+		std::cout << "Driving Maruthi Swift" << std::endl;
+	}
+};
+
+class SuzukiGixxer : public Vehicle
+{
+public:
+	void useVehicle()
+	{
+		std::cout << "Driving Suzuki Gixxer" << std::endl;
+	}
+};
+
+class HondaActiva : public Vehicle
+{
+public:
+	void useVehicle()
+	{
+		std::cout << "Driving Honda Activa" << std::endl;
+	}
+	
+};
+
+class TransportMode
+{
+public:
+	virtual void use() = 0;
+};
+
+class VehicularTransportMode : public TransportMode
+{
+public:
+	std::unique_ptr<Vehicle> vehicle;
+	VehicularTransportMode(Vehicle *v): vehicle(v) {}
+	virtual void use()
+	{
+		vehicle->useVehicle();
+	}
+};
+
+class TrainTransportMode : public TransportMode
+{
+public:
+	virtual void use()
+	{
+		std::cout << "Riding in a train" << std::endl;
+	}
+};
+
+class PedestrianTransportMode : public TransportMode
+{
+public:
+	virtual void use()
+	{
+		std::cout << "Walking by foot" << std::endl;
+	}
+};
+
+class FamilyMember
+{
+private:
+	std::unique_ptr<TransportMode> transportMode;
+public:
+	virtual void display() = 0;
+	void setTransportMode(TransportMode *mode)
+	{
+		transportMode.reset(mode);
+	}
+	void transport()
+	{
+		transportMode->use();
+	}
+};
+
+class Father : public FamilyMember
+{
+public:
+	virtual void display()
+	{
+		std::cout << "I am father" << std::endl;
+	}
+};
+
+class Mother : public FamilyMember
+{
+public:
+	virtual void display()
+	{
+		std::cout << "I am mother" << std::endl;
+	}
+};
+
+class Brother : public FamilyMember
+{
+public:
+	virtual void display()
+	{
+		std::cout << "I am Brother" << std::endl;
+	}
+};
+
+class Sister : public FamilyMember
+{
+public:
+	virtual void display()
+	{
+		std::cout << "I am Sister" << std::endl;
+	}
+};
+
+int main()
+{
+	FamilyMember *father = new Father();
+	father->display();
+	father->setTransportMode(new PedestrianTransportMode());
+	father->transport();
+}
